048a09b9c450cc4338ff7fcac687cd09e59fabf6
jeffreylandes <jeffrey@spacemaker.ai>
2022-09-29 20:14:52 -0600

    chore: add simple Location, User classes

---
+++ user/Location.java
@@ -0,0 +1,10 @@
+package user;
+
+public class Location {
+    Float latitude;
+    Float longitude;
+    public Location(Float latitude, Float longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
---
+++ user/User.java
@@ -0,0 +1,17 @@
+package user;
+
+public class User {
+    String firstName;
+    String lastName;
+    Location location;
+
+    public User(String firstName, String lastName, Location location) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.location = location;
+    }
+
+    public void fetchWeather() {
+        
+    }
+}
